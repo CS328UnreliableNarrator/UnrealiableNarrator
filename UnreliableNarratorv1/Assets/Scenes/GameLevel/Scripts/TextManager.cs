016a1d430de30b50c640689ebeedbb0aa7d97879
5006845293 <jasminewells@nevada.unr.edu>
2022-10-23 14:50:59 -0700

    textbox

--- UnreliableNarratorv1/Assets/Scenes/GameLevel/Scripts/TextManager.cs
+++ UnreliableNarratorv1/Assets/Scenes/GameLevel/Scripts/TextManager.cs
@@ -6,14 +6,14 @@ using TMPro;
 public class TextManager : MonoBehaviour
 {
 	public GameObject textBox;
-	public TextMeshProUGUI Speech;
+	public Text Speech;
 	
 	public TextAsset textFile;
 	public string[] TextLines;
 	
 	public int currentLine;
 	public int endAtLine;
-	
+	string test = "hi";
 	//public PlayerController player;
     // Start is called before the first frame update
     void Start()
@@ -23,13 +23,20 @@ public class TextManager : MonoBehaviour
          if(textFile !=null){
 			TextLines = (textFile.text.Split('\n'));
 		}
+		
+		if(endAtLine == 0){
+			endAtLine = TextLines.Length - 1;
+		}
     }
     
-	void update(){
-		Speech.text = TextLines[currentLine].ToString();
-
+	void LateUpdate(){
+		
+		Speech.text = TextLines[currentLine];
 		if(Input.GetKeyDown(KeyCode.Return)){
-			currentLine += 1;
+			currentLine +=1;
+		}
+		if(currentLine>endAtLine){
+			textBox.SetActive(false);
 		}
 	}
 
