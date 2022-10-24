b66163b57202d21f069cc7e1d13c617edd980b34
andrewbeck319 <andrewbeck319@gmail.com>
2022-10-23 19:04:52 -0700

    Hooked Computer Email Puzzle to Camille's computer and made it unlock the Bossman's

--- UnreliableNarratorv1/Assets/Scenes/ComputerPuzzleScene/Scripts/ComputerPuzzleController.cs
+++ UnreliableNarratorv1/Assets/Scenes/ComputerPuzzleScene/Scripts/ComputerPuzzleController.cs
@@ -7,6 +7,9 @@ public class ComputerPuzzleController : MonoBehaviour
     public GameObject emailWindow;
     public GameObject controlPanelWindow;
     public GameObject menuStart;
+
+    [SerializeField] private string answer;
+    [SerializeField] private TMPro.TMP_InputField input;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +19,23 @@ public class ComputerPuzzleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(input.text == answer)
+        {
+            PlayerPrefs.SetInt("PCLock", 0);
+        }
     }
 
+    private void OnEnable()
+    {
+        Cursor.visible = true;
+
+
+    }
+
+    private void OnDisable()
+    {
+        Cursor.visible = false;
+    }
     public void ToggleEmailWindow()
     {
         this.emailWindow.SetActive(!this.emailWindow.activeSelf);
